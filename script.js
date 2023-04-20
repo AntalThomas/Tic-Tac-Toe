67cf0de9b691a62f8c568cb0f8ec6b2eb7e2f383
AntalThomas <Blake-Heward@hotmail.com>
2023-04-20 15:22:07 +0930

    Tic_Tac_Toe in Progress Updated README. Updated winners window now shows who won.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var winningWindow = document.querySelector("#winningWindow")
 var gameBoard = document.querySelector("#gameBoard")
 var gameCell = document.getElementsByClassName("gameCell")
+var winningPlayer = document.querySelector(".winningPlayer")
 const playerOne = "X", playerTwo = "O"
 var currentPlayer = playerOne
 var winningGames = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -25,6 +26,12 @@ function playerShot(event) {
 
     if (checkWinner(winningGames) === true) {
         winningWindow.style.visibility = "visible"
+
+        if (currentPlayer === "X") {
+            winningPlayer.textContent = "O"
+        } else {
+            winningPlayer.textContent = "X"
+        }
     }
 }
 
