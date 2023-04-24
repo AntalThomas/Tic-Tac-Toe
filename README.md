8c623cc43d3495bf43ae0021581dce4be62fbbca
AntalThomas <Blake-Heward@hotmail.com>
2023-04-24 10:21:15 +0930

    Tic_Tac_Toe in Progress Updated README Restart button now just resets the board. Scores are kept after a win. Theme box added ready for functionality. Next game button added after a win.

--- README.md
+++ README.md
@@ -24,23 +24,25 @@ Tic Tac Toe is a fun game played between two people taking in turns to try and p
 ![Flow Chart](./Images/flowChart.png)
 
 ## Extra Plan
-1. Add three themes to pick from at any time at the top left corner of screen.
-2. Restart button doesn't refresh the page it just resets the game board.
-3. Scores are kept for each player.
+1. Scores are kept for each player.
+2. Add three themes to pick from at any time at the top left corner of screen.
+3. Restart button doesn't refresh the page it just resets the game board.
 4. Player chooses symbols before game starts.
 
 ## Struggles:
 I really struggled with making the event handler take it in turns with applying either a "X" or "O", so for a work around until I figured out how to do that, I just made the user cycle through the options with a click. I eventually found help on stack overflow with a similar problem that helped me.
 
 ## Bugs to Fix
-- Restart refreshes the page instead of resetting everything.
-- When a player has won the players can still click on blank cells.
+- Restart refreshes the page instead of resetting everything.  :white_check_mark:
+- When a player has won the players can still click on blank cells. :white_check_mark:
 - Make prettier.
 - Improve code readability.
+- Since the whole page resets the scores always reset to 0  :white_check_mark:
+- The last players score updates by one if the game is drawn
 
 ## Future Features
-- Next game button after winner is awarded.
-- Keep scores between players.
+- Next game button after winner is awarded.  :white_check_mark:
+- Keep scores between players.  :white_check_mark:
 - Player chosen symbol.
 - Different themes that can be chosen at any time.
 
@@ -58,3 +60,5 @@ https://gist.git.generalassemb.ly/kasun/24d22950b4e29cd275f3e62bc77ccaa1
 https://stackoverflow.com/questions/29884654/button-that-refreshes-the-page-on-click
 
 https://www.w3schools.com/jsref/met_element_remove.asp
+
+https://stackoverflow.com/questions/56418763/creating-the-perfect-rainbow-gradient-in-css
--- script.js
+++ script.js
@@ -3,10 +3,34 @@ var gameBoard = document.querySelector("#gameBoard")
 var gameCell = document.getElementsByClassName("gameCell")
 var winningPlayer = document.querySelector(".winningPlayer")
 var restartButton = document.querySelector("#restart")
+var nextGameButton = document.querySelector("#nextGame")
+var playerOneScoreCard = document.querySelector(".playerOneScore")
+var playerTwoScoreCard = document.querySelector(".playerTwoScore")
 const playerOne = "X", playerTwo = "O"
+var playerOneScore = 0, playerTwoScore = 0
 var currentPlayer = playerOne
 var game = true
 var winningGames = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+function updateScores() {
+    playerOneScoreCard.textContent = playerOneScore
+    playerTwoScoreCard.textContent = playerTwoScore
+}
+
+function resetGameBoard() {
+    gameBoard.style.visibility = "visible"
+
+    for (var i = 0; i < 9; i++) {
+        gameBoard.children[i].textContent = ""
+        gameBoard.children[i].classList.remove("X")
+        gameBoard.children[i].classList.remove("O")
+    }
+
+    nextGameButton.style.visibility = "hidden"
+    winningWindow.style.visibility = "hidden"
+    drawnWindow.style.visibility = "hidden"
+
+}
 
 function playerShot(event) {
     var clickedCell = event.target
@@ -30,15 +54,19 @@ function playerShot(event) {
 
         if (currentPlayer === "X") {
             winningPlayer.textContent = "O"
+            updateScores()
         } else {
             winningPlayer.textContent = "X"
+            updateScores()
         }
 
-        gameBoard.remove()
+        gameBoard.style.visibility = "hidden"
+        nextGameButton.style.visibility = "visible"
     }
 
     if (checkDrawn(gameBoard) === true) {
         drawnWindow.style.visibility = "visible"
+        gameBoard.style.visibility = "hidden"
     }
 }
 
@@ -73,12 +101,21 @@ function checkWinner(winningGames) {
     }
 
     for (var i = 0; i < winningGames.length; i++) {
-        if (loopChecker("X") === true) { return true }
+        if (loopChecker("X") === true) { 
+            playerOneScore++
+            return true 
+        }
 
-        if (loopChecker("O") === true) { return true }
+        if (loopChecker("O") === true) { 
+            playerTwoScore++
+            return true 
+        }
     }
 }
 
 for (var i = 0; i < gameCell.length; i++) {
     gameCell[i].addEventListener("click", playerShot)
 }
+
+restartButton.addEventListener("click", resetGameBoard)
+nextGameButton.addEventListener("click", resetGameBoard)
